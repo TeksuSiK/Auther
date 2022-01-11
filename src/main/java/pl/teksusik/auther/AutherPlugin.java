a846fb90061ce3becfecaa5ffe30fe0143aa7e89
TeksuSiK <ntuspg@gmail.com>
2022-01-11 21:09:37 +0100

    Initial commit

---
+++ src/main/java/pl/teksusik/auther/AutherPlugin.java
@@ -0,0 +1,13 @@
+package pl.teksusik.auther;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutherPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}

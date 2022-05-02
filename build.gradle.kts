6ca647669edab969a7040b11fb335473bf710e6b
TeksuSiK <oskar@teksusik.pl>
2022-05-02 15:19:16 +0200

    Add Paper as a dependency, basic JavaPlugin structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,9 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
 }
 
 dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
 }
